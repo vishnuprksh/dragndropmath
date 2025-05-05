5964908de8d84dce8fe84b20cac02014c59a3760
Vishnu Prakash <119521719+vishnuprksh@users.noreply.github.com>
2025-05-05 11:08:50 +0000

    Feature: Add Express server and update package.json configuration

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// All routes should serve the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
